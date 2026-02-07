282a3a6dab64976052b4a63190db8102a6584df3
FoarteBine <sasabelous332@gmail.com>
2026-02-07 22:01:00 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,10 @@
+local success, result = pcall(function()
+    return game:HttpGet("https://raw.githubusercontent.com/FoarteBine/MdFuncs/refs/heads/main/loader.lua")
+end)
 
+if success then
+    loadstring(result)()
+    if getgenv().MdFuncs then
+        getgenv().MdFuncs:LoadModule("https://raw.githubusercontent.com/FoarteBine/MdFuncs/refs/heads/main/visuals.lua")
+    end
+end
