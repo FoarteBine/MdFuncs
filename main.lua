43728c796638a407b3619d3f9666833b39d52a03
FoarteBine <sasabelous332@gmail.com>
2026-03-23 21:29:29 +0200

    Add loading of additional MdFuncs modules

--- main.lua
+++ main.lua
@@ -5,6 +5,8 @@ end)
 if success then
     loadstring(result)()
     if getgenv().MdFuncs then
+        getgenv().MdFuncs:LoadModule("https://raw.githubusercontent.com/FoarteBine/MdFuncs/refs/heads/main/game.lua")
+        getgenv().MdFuncs:LoadModule("https://raw.githubusercontent.com/FoarteBine/MdFuncs/refs/heads/main/localplayer.lua")
         getgenv().MdFuncs:LoadModule("https://raw.githubusercontent.com/FoarteBine/MdFuncs/refs/heads/main/visuals.lua")
     end
 end
